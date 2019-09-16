a2d3b80b8b2bdd8d163d7dd9d1ff0e4dfbc061ba
bryant <bryantkrause@gmail.com>
2019-09-15 22:59:28 -0700

    this is the final countdown

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -5,3 +5,18 @@ function myFunction() {
     var a = answerKey.indexOf("Answer1");
     document.getElementById("question1").innerHTML = a;
   }
+
+  var timeLeft = 3;
+  var elem = document.getElementById('some_div');
+  
+  var timerId = setInterval(countdown, 1000);
+  
+  function countdown() {
+    if (timeLeft === 0) {
+      clearTimeout(timerId);
+      doSomething();
+    } else {
+      elem.innerHTML = timeLeft + ' seconds remaining';
+      timeLeft--;
+    }
+  }
