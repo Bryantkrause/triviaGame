774a67d3873d6e7d4149982589c681b0fce2d2f0
Bryant Krause <bryantkrause@gmail.com>
2019-09-17 18:04:52 -0700

    changed design made it look prettier

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -1,13 +1,14 @@
 // these are the answers
 var answerKey = ["Answer1","Answer2","Answer3"]
+ var timeLeft = 3;
+  var elem = document.getElementById('timer');
+
 
 function myFunction() {
     var a = answerKey.indexOf("Answer1");
     document.getElementById("question1").innerHTML = a;
   }
-
-  var timeLeft = 3;
-  var elem = document.getElementById('some_div');
+ 
   
   var timerId = setInterval(countdown, 1000);
   
@@ -21,12 +22,7 @@ function myFunction() {
     }
   }
 
-  // on click make a box
-
-  var boxinator = function boxy() {
-
-
-  }
+ 
 
   // this creates a button
   function myButton() {
@@ -36,7 +32,7 @@ function myFunction() {
   }
 
 
-  psuedo
+  
   // once the last button has been pressed it splits into to buttons
   // click the most buttons to get to a high score
   
