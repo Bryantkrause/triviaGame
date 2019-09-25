71273906cb1fbd0fbbba2e409bca3b9c204a03ee
Bryant Krause <bkrause@ttdwest.local>
2019-09-25 15:25:54 -0700

    testing updates

--- assets/JS/game.js
+++ assets/JS/game.js
@@ -2,7 +2,7 @@
 var answerKey = ["Answer1","Answer2","Answer3"]
  var timeLeft = 3;
   var elem = document.getElementById('timer');
-
+document.getElementById("quiz").style.display === 'none'
 
 function myFunction() {
     var a = answerKey.indexOf("Answer1");
@@ -31,7 +31,14 @@ function myFunction() {
     document.body.appendChild(btn);
   }
 
-
+  function startoElQuizo() {
+    var x = document.getElementById("quiz");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
   
   // once the last button has been pressed it splits into to buttons
   // click the most buttons to get to a high score
